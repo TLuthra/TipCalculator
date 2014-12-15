90fd52e746a3b0f4a35d6651fcbab68e304e3aff
Tanooj Luthra <tluthra@box.com>
2014-12-14 23:53:19 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Tanooj Luthra on 12/14/14.
+//  Copyright (c) 2014 Tanooj Luthra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Tanooj Luthra on 12/14/14.
+//  Copyright (c) 2014 Tanooj Luthra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
